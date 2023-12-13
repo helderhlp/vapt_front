6f522047f69710c4cfd3c85779e4b091979d5cad
Guilherme Breda <guilherme.breda@sejaefi.com.br>
2023-12-13 20:42:06 -0300

    fix: Corrige erros de formatação

--- src/services/ApiClientService.js
+++ src/services/ApiClientService.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const ApiClient = axios.create({
   baseURL: `${import.meta.env.VITE_APP_API_URL}api/v1`,
   headers: {
     'Content-type': 'application/json',
     'Access-Token': import.meta.env.VITE_APP_ACCESS_TOKEN
-  },
-});
+  }
+})
 
-export default ApiClient;
+export default ApiClient
